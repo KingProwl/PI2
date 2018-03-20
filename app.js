806619d0d85a50706d23fd0a8a9c27574938b804
KingProwl <paul.ghafari@wanadoo.fr>
2018-03-20 11:29:10 +0100

    CSS applied on missing pages :milky_way:

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var nunjucks = require('nunjucks');
 var bodyParser = require('body-parser');
 var multer = require('multer');
-
+var path = require('path')
 
 
 var upload = multer({
@@ -23,7 +23,7 @@ app.use(upload.single('file'));
 app.use('/css',express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use('/', require('./routes/clients'));
 app.use('/types',require('./routes/types'));
-app.use(express.static(__dirname + '/public'));
+app.use('/:r', express.static(path.join(__dirname, 'public')));
 app.use('/uploads',express.static(__dirname + '/uploads'));
 
 nunjucks.configure('views', {
