76896061cf6002f21851cc6f36ea31073eed6ede
KingProwl <paul.ghafari@wanadoo.fr>
2018-03-27 17:09:03 +0200

    Updated Type Models JSON :fire:

--- models/Type.js
+++ models/Type.js
@@ -2,6 +2,26 @@ var mongoose = require('mongoose');
 
 var typeSchema = new mongoose.Schema({
     name : String,
+    ISIN : String,
+    forme_juridique : String,
+    famille : String,
+    AMF : String,
+    numero_agrement : String,
+    PEA1 : String,
+    PEA2 : String,
+    societe : String,
+    date_creation : String,
+    actif : String,
+    Sharpe : String,
+    P1W : String,
+    P01 : String,
+    P1M : String,
+    P3M : String,
+    P6M : String,
+    P1Y : String,
+    P3Y : String,
+    P5Y : String,
+    P10Y : String,
     color : {
         type : String,
         default : 'red'
